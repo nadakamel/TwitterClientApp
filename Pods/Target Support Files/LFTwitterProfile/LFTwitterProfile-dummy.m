90fdfe73ffbf5670e34dd805bc2f6ca401ad3659
Nada Kamel <nadakamel9@hotmail.com>
2021-02-03 18:35:47 +0200

    Delete Pods directory

--- Pods/Target Support Files/LFTwitterProfile/LFTwitterProfile-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_LFTwitterProfile : NSObject
-@end
-@implementation PodsDummy_LFTwitterProfile
-@end
--- Pods/Target Support Files/Pods-Twitter Client App/Pods-Twitter Client App-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Twitter_Client_App : NSObject
-@end
-@implementation PodsDummy_Pods_Twitter_Client_App
-@end
